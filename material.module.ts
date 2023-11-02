74d7b7f780f0f6a9dd08f1a83da564736b94ec3f
Sahithi-Lakkakula31 <147141618+Sahithi-Lakkakula31@users.noreply.github.com>
2023-11-02 20:43:37 +0530

    Add files via upload

---
+++ material.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { MatPaginatorModule } from '@angular/material/paginator';
+@NgModule({
+    declarations:[],
+    imports:[MatPaginatorModule],
+    exports:[MatPaginatorModule],
+    providers: [
+      // Add any services that the module will provide
+    ]
+  })
+  export class MyModule { }
